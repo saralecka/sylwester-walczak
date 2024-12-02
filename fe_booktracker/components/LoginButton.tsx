b42b02c863ef16ed597ef194f58584289d72cf65
Sylwester Walczak <swalczak94@gmail.com>
2024-12-02 14:49:19 +0100

    major_update

--- fe_booktracker/components/LoginButton.tsx
+++ fe_booktracker/components/LoginButton.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function LoginButton() {
   const { data: session } = useSession();
+  const router = useRouter();
 
   if (session) {
     return <Button onClick={() => signOut()}>Wyloguj</Button>;
   }
-  return <Button onClick={() => signIn()}>Zaloguj</Button>;
+
+  return (
+    <Button
+      onClick={() => {
+        router.push("/login");
+      }}
+    >
+      Zaloguj
+    </Button>
+  );
 }
